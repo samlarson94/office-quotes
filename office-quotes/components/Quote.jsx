ffc2f0a26e76b2cfce3fc838c0cb2750dec09cd1
samlarson94 <samlarson94@gmail.com>
2023-03-27 19:13:23 -0500

    Debugging fetch call in useEffect.

--- office-quotes/components/Quote.jsx
+++ office-quotes/components/Quote.jsx
@@ -1,26 +1,25 @@
 import React, { useState, useEffect } from 'react';
 
-const defaultEndpoint = 'https://officeapi.dev/api/quotes/random';
+
 
 function Quote() {
-  const [quote, setQuote] = useState(null);
+//   const defaultEndpoint = ;
+  let [quote, setQuote] = useState(null);
 
   // fetch quote data and set it to the state variable
   useEffect(() => {
     console.log("TRYING TO CONNECT")
-    fetch(defaultEndpoint)
-    console.log("DATA FETCHED")
-      .then(response => response.json())
-      console.log("TRANSLATING TO JSON")
-      .then(data => setQuote(data));
+    fetch("https://officeapi.dev/api/quotes/random")
+    .then(response => response.json())
+    .then(data => setQuote(data));
   }, []);
 
   function fetchQuote() {
-    fetch(defaultEndpoint)
+    fetch("https://officeapi.dev/api/quotes/random")
       .then(response => response.json())
       .then(data => setQuote(data));
   }
-  console.log("trying to fetch quote" + quote)
+//   console.log("trying to fetch quote" + quote)
   return (
     <div>
       {quote && (
